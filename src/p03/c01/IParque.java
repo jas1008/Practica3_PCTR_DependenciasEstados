1bebf5effbf0e51c7a403c03967eb831f3f8780a
Juan <jas1008@alu.ubu.es>
2021-04-14 18:46:31 +0200

    Arreglos finales y comentarios.
    Arreglados bugs de estado de monitor, errores en los invariantes, y
    comentada la clase Parque con Javadoc.

--- src/p03/c01/IParque.java
+++ src/p03/c01/IParque.java
@@ -5,5 +5,4 @@ public interface IParque {
 	public abstract void entrarAlParque(String puerta);
 
 	public abstract void salirDelParque(String puerta);
-
 }
--- src/p03/c01/Parque.java
+++ src/p03/c01/Parque.java
@@ -3,16 +3,35 @@ package p03.c01;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
+/**
+ * Clase que representa un parque con varias puertas
+ * y regula su comportamiento con concurrencia y
+ * exclusión mutua.
+ * @author Daniel Alonso Báscones
+ * @author Juan Abelairas Soto-Largo
+ *
+ */
 public class Parque implements IParque{
 
-
-	// TODO 
-	
+	/**
+	 * Aforo del parque.
+	 */
 	private int aforo;
+	
+	/**
+	 * Contador de personas en total.
+	 */
 	private int contadorPersonasTotales;
+	
+	/**
+	 * Contadores de las puertas.
+	 */
 	private Hashtable<String, Integer> contadoresPersonasPuerta;
 	
-	
+	/**
+	 * Constructor de parque.
+	 * @param aforo Aforo del parque.
+	 */
 	public Parque( int aforo ) { 
 		contadorPersonasTotales = 0;
 		contadoresPersonasPuerta = new Hashtable<String, Integer>();
@@ -21,10 +40,10 @@ public class Parque implements IParque{
 
 
 	@Override
-	synchronized public void entrarAlParque(String puerta){
+	synchronized public void entrarAlParque(String puerta) {
 		
 		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
+		if (contadoresPersonasPuerta.get(puerta) == null) {
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
 		
@@ -39,32 +58,38 @@ public class Parque implements IParque{
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Entrada");
 		
+		// Comprobamos el invariante
 		checkInvariante();
-		notify();
+		this.notifyAll();
 	}
 	
 	@Override
-	public void salirDelParque(String puerta) {
-		// Si no hay entradas por esa puerta, inicializamos
-		if (contadoresPersonasPuerta.get(puerta) == null){
+	public synchronized void salirDelParque(String puerta) {
+		// Si no hay salidas por esa puerta, inicializamos
+		if (contadoresPersonasPuerta.get(puerta) == null) {
 			contadoresPersonasPuerta.put(puerta, 0);
 		}
 		
 		// Llamada al metodo de comprobacion
 		comprobarAntesDeSalir();
 		
-		
 		// Aumentamos el contador total y el individual
-		contadorPersonasTotales--;		
+		contadorPersonasTotales--;
 		contadoresPersonasPuerta.put(puerta, contadoresPersonasPuerta.get(puerta) - 1);
 		
 		// Imprimimos el estado del parque
 		imprimirInfo(puerta, "Salida");
 		
+		// Comprobamos el invariante
 		checkInvariante();
-		notify();
+		this.notifyAll();
 	}
 	
+	/**
+	 * Imprime la información.
+	 * @param puerta Puerta por la que sucede el evento.
+	 * @param movimiento Movimiento realizado.
+	 */
 	private void imprimirInfo (String puerta, String movimiento){
 		System.out.println(movimiento + " por puerta " + puerta);
 		System.out.println("--> Personas en el parque " + contadorPersonasTotales); //+ " tiempo medio de estancia: "  + tmedio);
@@ -76,6 +101,10 @@ public class Parque implements IParque{
 		System.out.println(" ");
 	}
 	
+	/**
+	 * Suma los contadores de las puertas.
+	 * @return Suma total
+	 */
 	private int sumarContadoresPuerta() {
 		int sumaContadoresPuerta = 0;
 			Enumeration<Integer> iterPuertas = contadoresPersonasPuerta.elements();
@@ -85,12 +114,14 @@ public class Parque implements IParque{
 		return sumaContadoresPuerta;
 	}
 	
+	/**
+	 * Comprueba los invariantes.
+	 */
 	protected void checkInvariante() {
 		
-		
-		assert sumarContadoresPuerta() == contadorPersonasTotales : "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
-		// TODO 
-		// TODO
+		assert sumarContadoresPuerta() == contadorPersonasTotales: "INV: La suma de contadores de las puertas debe ser igual al valor del contador del parte";
+		assert sumarContadoresPuerta() >= 0: "El parque está vacío";
+		assert sumarContadoresPuerta() <= aforo: "El parque está lleno";
 	}
 
 	/**
@@ -98,11 +129,10 @@ public class Parque implements IParque{
 	 */
 	protected void comprobarAntesDeEntrar(){	// TODO
 		
-		
-		while ( contadorPersonasTotales == aforo  ) {
+		while ( contadorPersonasTotales >= aforo ) {
 			
 			try {
-				wait();
+				this.wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
@@ -111,17 +141,18 @@ public class Parque implements IParque{
 		
 	}
 
+	/**
+	 * Este metodo se encarga de dormir el hilo si el parque esta vacío
+	 */
 	protected void comprobarAntesDeSalir(){		// TODO
-		while ( contadorPersonasTotales == 0  ) {
+		while ( contadorPersonasTotales <= 0 ) {
 			
 			try {
-				wait();
+				this.wait();
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			
 		}	
 	}
-
-
 }
